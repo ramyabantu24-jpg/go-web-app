b039f504376baaa32e2140159c3a631b113e0d20
ramyabantu24-jpg <ramyabantu24@gmail.com>
2026-05-16 14:27:48 +0530

    updated change 2

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/home.html")
 }
 
-
 func coursePage(w http.ResponseWriter, r *http.Request) {
 	// Render the course html page
 	http.ServeFile(w, r, "static/courses.html")
